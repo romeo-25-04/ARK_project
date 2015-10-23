0e72dec5d293ce646a6991d2102d86826360b2ae
Roman Capsamun <r.capsamun@campus.lmu.de>
2015-10-23 19:30:23 +0200

    Eine Zeile Fertig

--- genetics.php
+++ genetics.php
@@ -12,7 +12,7 @@
     	<th>Have Female</th>
     </tr>
     <tr>
-        <td class="icon">icon</td>
+        <td class="icon"><img src="./img/Herbivore/30px-Ankylo_Icon.png" /></td>
         <td class="name">Ankylo</td>
         <td class="genM"><input type="checkbox" name="gender" value="male" /></td>
         <td class="genF"><input type="checkbox" name="gender" value="female" /></td>
