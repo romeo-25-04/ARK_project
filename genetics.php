d40558459b6e784b1f1e0b43bdb6e160aef74fa2
Roman Capsamun <r.capsamun@campus.lmu.de>
2015-10-23 19:16:15 +0200

    renamed html to php

---
+++ genetics.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>ARK Genetics</title>
+</head>
+
+<body>
+<h1>ARK Genetics</h1>
+<table border="1" style="width:100%">
+    <tr>
+    	<th colspan="2">Dino Name</th>
+    	<th>Have Male</th>
+    	<th>Have Female</th>
+    </tr>
+    <tr>
+        <td class="icon">icon</td>
+        <td class="name">Ankylo</td>
+        <td class="genM"><input type="checkbox" name="gender" value="male" /></td>
+        <td class="genF"><input type="checkbox" name="gender" value="female" /></td>
+    </tr>
+</table>
+
+</body>
+
+</html>
